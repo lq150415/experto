92661d71513d0f13d15810b993c4bdef3c21dbf0
lq150415 <lq150415@gmail.com>
2016-06-10 00:50:20 -0400

    Cribaje

--- resources/views/perfil-diagnostico.blade.php
+++ resources/views/perfil-diagnostico.blade.php
@@ -4,9 +4,23 @@
   <div class="alert alert-info">Paciente - Perfil </div>
   <div class="alert panel panel-success cuerpo" style="background:#fff; margin-top:-2.7%">
   <div class="form-group">
-    <label for="ejemplo_email_3" class="col-lg-1 control-label label label-primary">Paciente :</label>
-    <div class="col-lg-9">
+    <label for="ejemplo_email_3" class="col-lg-3 control-label label label-primary">Paciente :</label>
+    <div class="col-lg-6">
       <input type="text" readonly name="estatura"  class="form-control" id="" value="<?php echo $perfil->NomPas.' '.$perfil->PatMat.' '.$perfil->MatPas;?>">
+    </div>
+    </div>
+    <br/>
+    <div class="form-group">
+    <label for="ejemplo_email_3" class="col-lg-3 control-label label label-primary">Fecha de nacimiento :</label>
+    <div class="col-lg-6">
+      <input type="text" readonly name="estatura"  class="form-control" id="" value="<?php echo $perfil->FecNacPas;?>">
+    </div>
+    </div>
+    <br/>
+    <div class="form-group">
+    <label for="ejemplo_email_3" class="col-lg-3 control-label label label-primary">Telefono :</label>
+    <div class="col-lg-6">
+      <input type="text" readonly name="estatura"  class="form-control" id="" value="<?php echo $perfil->TelPas;?>">
     </div>
     </div>
   </div>
